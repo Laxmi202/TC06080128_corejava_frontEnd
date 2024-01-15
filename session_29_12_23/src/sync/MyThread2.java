582d796be9824834988909fc73a1a49fc8e22746
ganes <ganes@Lenovo_Idepad>
2024-01-15 09:47:24 +0530

    Commit

---
+++ session_29_12_23/src/sync/MyThread2.java
@@ -0,0 +1,17 @@
+package sync;
+
+public class MyThread2 extends Thread
+{
+	Factorial f;
+	
+	public MyThread2(Factorial f)
+	{
+		super();
+		this.f = f;
+	}
+	public void run()
+	{
+		f.fact(7);
+	}
+
+}
